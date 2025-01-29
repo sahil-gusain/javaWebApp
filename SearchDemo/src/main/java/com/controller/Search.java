d6d26a48a6e5f9a3adfd63a1b31fcb04d5c5b235
sahil-gusain <sahilgusainddn@gmail.com>
2025-01-29 21:27:36 +0530

    started searchDemo

---
+++ SearchDemo/src/main/java/com/controller/Search.java
@@ -0,0 +1,49 @@
+package com.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Search
+ */
+@WebServlet("/Search")
+public class Search extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Search() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    System.out.println("form submitted");
+	    String searchtype = request.getParameter("searchtype");
+	    response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("<h2>hello world</h2> "+ searchtype);
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+        pw.close();
+		doGet(request, response);
+	}
+
+}
